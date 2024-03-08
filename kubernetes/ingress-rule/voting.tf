b99025779e6ee6bdc0059ed23e8ac1d6d13ef21e
Teni <tshodeinde96@gmail.com>
2024-03-08 14:51:46 +0100

    changes

--- kubernetes/ingress-rule/voting.tf
+++ kubernetes/ingress-rule/voting.tf
@@ -12,7 +12,7 @@ resource "kubernetes_ingress_v1" "voting-ingress" {
 
   spec {
     rule {
-      host = "voting-app.nwokolo.live"
+      host = "voting-app.awspro.com.ng"
       http {
         path {
           backend {
@@ -44,7 +44,7 @@ resource "kubernetes_ingress_v1" "result-ingress" {
 
   spec {
     rule {
-      host = "result.nwokolo.live"
+      host = "result.awspro.com.ng"
       http {
         path {
           backend {
